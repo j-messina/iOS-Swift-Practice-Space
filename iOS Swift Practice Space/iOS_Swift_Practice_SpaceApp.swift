3a8a9886a80ca74b8fd50e51665b35d4c0269e73
John Messina <j.w.messina24@gmail.com>
2023-08-20 17:19:04 -0700

    Initial Commit

---
+++ iOS Swift Practice Space/iOS_Swift_Practice_SpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Swift_Practice_SpaceApp.swift
+//  iOS Swift Practice Space
+//
+//  Created by John Messina on 8/20/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Swift_Practice_SpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
